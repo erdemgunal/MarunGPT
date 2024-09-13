d030b88cbf2844cfd8dd6664552fc7b5169b5804
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2024-09-13 15:38:01 +0300

    chore: Update NotFound component with responsive text size

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,8 +4,8 @@ import { Button } from "@/components/ui/button";
 export default function NotFound() {
   return (
     <main className="flex h-96 flex-col items-center justify-center bg-black text-white p-6">
-      <h2 className="text-8xl tracking-tight">404</h2>
-      <p className="textsm mt-4 text-center max-w-md">
+      <h2 className="text-6xl sm:text-8xl tracking-tight">404</h2>
+      <p className="text-sm sm:text-lg mt-4 text-center max-w-md">
         The page you're trying to access cannot be found. You might have entered the address incorrectly, or the page may have been relocated.
       </p>
       <Link href="/">
