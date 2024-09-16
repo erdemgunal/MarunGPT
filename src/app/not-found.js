be5b1a17c69106f7a83c2caa500c590c208d14d0
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2024-09-16 09:46:41 +0300

    chore: Update NotFound component with full screen height

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <main className="flex h-96 flex-col items-center justify-center bg-black text-white p-6">
+    <main className="flex h-screen flex-col items-center justify-center bg-black text-white p-6">
       <h2 className="text-6xl sm:text-8xl tracking-tight">404</h2>
       <p className="text-sm sm:text-lg mt-4 text-center max-w-md">
     The page you&apos;re trying to access cannot be found. You might have entered the address incorrectly, or the page may have been relocated.
